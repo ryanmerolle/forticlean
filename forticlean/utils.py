92605dbc0fe2b354c0df4cabfacd480560306754
ryanmerolle <ryan.merolle@gmail.com>
2023-07-20 21:01:03 +0000

    move from coloredlogs to rich.logging

--- forticlean/utils.py
+++ forticlean/utils.py
@@ -1,21 +1,23 @@
 import logging
 from pathlib import Path
 
-import coloredlogs
+from rich.logging import RichHandler
 
 # Logging setup
-logging.basicConfig(level=logging.INFO)
-logging.getLogger(name="FortiCleaner")
-coloredlogs.install(logging=logging)
+logging.basicConfig(
+    level=logging.INFO, format="%(message)s", datefmt="[%X]", handlers=[RichHandler()]
+)
+
+logger = logging.getLogger("FortiCleaner")
 
 
 def read_file(file_path_to_read_from: str) -> str:
     config_file = Path(file_path_to_read_from)
     if config_file.exists():
-        logging.info(f"File '{file_path_to_read_from}' opened successfully")
+        logger.info(f"File '{file_path_to_read_from}' opened successfully")
         return config_file.read_text(encoding="utf-8")
     else:
-        logging.error(f"Error: File '{file_path_to_read_from}' not found")
+        logger.error(f"Error: File '{file_path_to_read_from}' not found")
         return ""
 
 
@@ -23,8 +25,6 @@ def write_file(content: list[str], file_path_to_write_to: str):
     out_file = Path(file_path_to_write_to)
     try:
         out_file.write_text("\n".join(content), encoding="utf-8")
-        logging.info(f"File '{file_path_to_write_to}' written successfully")
+        logger.info(f"File '{file_path_to_write_to}' written successfully")
     except PermissionError:
-        logging.error(
-            f"Error: Permission denied to open file '{file_path_to_write_to}'"
-        )
+        logger.error(f"Error: Permission denied to open file '{file_path_to_write_to}'")
